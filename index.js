eae866a099d86a71be3e2778dde9e9e29ff22935
Dede Iskandar <d3215k@gmail.com>
2023-02-24 15:11:35 +0700

    add example

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import puppeteer from "puppeteer";
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  //1. Execute function to get lead_story
+  const lead_story = await crawlSite();
+
+  //2. Show lead_story
+  res.send(`<html><body>${lead_story}</body></html>`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+async function crawlSite() {
+  console.log("Function Crawl Site executed");
+
+  //1. Set time and date for screenshot
+  const time = new Date();
+  const timestamp = time.getTime();
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 800 });
+  await page.goto("https://www.indokoding.com/");
+
+  //2. Take a screenshot
+  await page.screenshot({
+    path: `storage/images/${timestamp}.png`,
+    fullPage: true,
+  });
+
+  const lead_story = await page.$eval(
+    "#comp-ifmduswg > p:nth-child(1) > span > span",
+    (el) => el.innerHTML
+  );
+
+  return lead_story;
+}
